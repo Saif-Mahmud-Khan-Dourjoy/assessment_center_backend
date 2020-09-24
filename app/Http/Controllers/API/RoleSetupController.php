b23c0159335c495cf4581d8995d9aabc6ef29af1
mahabubislam <md.mahabubislam@hotmail.com>
2020-09-24 16:10:09 +0600

    add dynamic role setup option

---
+++ app/Http/Controllers/API/RoleSetupController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\RoleSetup;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleSetupController extends Controller
+{
+    public $successStatus = 200;
+    public $failedStatus = 500;
+    public $invalidStatus = 400;
+    function __construct()
+    {
+        //
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        $role_setup = RoleSetup::all();
+        return response()->json(['success' => true, 'role_setup' => $role_setup], $this-> successStatus);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'contributor_role_id' => 'required',
+            'new_register_user_role_id' => 'required',
+        ]);
+        $input = $request->all();
+
+        if( !Role::find($input['contributor_role_id']) ){
+            return response()->json(['success' => false, 'message' => 'Contributor Role not found'], $this->failedStatus);
+        }
+        if( !Role::find($input['new_register_user_role_id']) ){
+            return response()->json(['success' => false, 'message' => 'Register User Role not found'], $this->failedStatus);
+        }
+
+        $role_setup = RoleSetup::first();
+        $data = [
+            'contributor_role_id' => $input['contributor_role_id'],
+            'new_register_user_role_id' => $input['new_register_user_role_id'],
+        ];
+        if( $role_setup ){
+            $role_setup->update($data);
+        }
+        else{
+            $role_setup = RoleSetup::create($data);
+        }
+
+        if( $role_setup )
+            return response()->json(['success' => true, 'role_setup' => $role_setup], $this->successStatus);
+        else
+            return response()->json(['success' => false, 'message' => 'Setup Role added fail'], $this->failedStatus);
+    }
+}
---
+++ database/migrations/2020_09_22_062906_create_role_setups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleSetupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_setups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('contributor_role_id')->unsigned();
+            $table->foreign('contributor_role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('new_register_user_role_id')->unsigned();
+            $table->foreign('new_register_user_role_id')->references('id')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_setups');
+    }
+}
