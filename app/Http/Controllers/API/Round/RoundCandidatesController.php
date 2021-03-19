777223c9b88cf414b0a46bb8b9b74e56544404d9
Hemayet Ulllah <hemayet.nirjhoy@gmail.com>
2021-03-19 12:25:24 +0600

    Round canidates controller updated with logs

--- app/Http/Controllers/API/Round/RoundCandidatesController.php
+++ app/Http/Controllers/API/Round/RoundCandidatesController.php
@@ -18,6 +18,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use mysql_xdevapi\Exception;
 use phpDocumentor\Reflection\Types\Null_;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Cache as CacheCandidates;
@@ -36,12 +37,15 @@ class RoundCandidatesController extends Controller
     }
 
     public function index(Request $request){
-        $this->out->writeln('Fetching Round Candidates...');
-        $roundCandidates = RoundCandidates::all();
-        if($roundCandidates){
+        try{
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetching Round candidates.");
+            $roundCandidates = RoundCandidates::all();
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetching all round-candidates successful!");
             return response()->json(['success'=>true, 'round-candidates'=>$roundCandidates],$this->successStatus);
+        }catch (\Exception $e){
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to fetch round-candidates! error: ".$e->getMessage());
+            return response()->json(['success'=>false, "message"=>"Failed to fetch round-candidates!", "error"=>$e->getMessage()], $this->failedStatus);
         }
-        return response()->json(['success'=>false, 'message'=>'Unable to fetch round with candidates'], $this->failedStatus);
     }
 
 
@@ -52,13 +56,12 @@ class RoundCandidatesController extends Controller
 
     public function fresherCandidates(){
         try{
-            $this->out->writeln("Fetching Fresher-students who currently not in any round!");
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetching fresher-students who currently not in any round.");
             $user= UserProfile::where('user_id','=',Auth::id())->first();
             $students = UserProfile::with('student')->where('institute_id','=',$user->institute_id)->where('id','!=',$user->id)->get();
             $new_student = [];
             $roleList = RoleSetup::first();
             $roleName = Role::where('id','=',$roleList->student_role_id)->first();
-            $this->out->writeln('role name: '.$roleName->name);
             foreach ($students as $student) {
                 if(User::with(['user_profile'])->where('id','=',$student->user_id)->role($roleName->name)->first()){
                     if(RoundCandidates::where('student_id','=',$student->id)->exists()){
@@ -67,23 +70,22 @@ class RoundCandidatesController extends Controller
                     array_push($new_student, $student);
                 }
             }
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetching fresher student successful.");
             return response()->json(['success'=>true, 'students'=>$new_student],$this->successStatus);
         }catch(\Exception $e){
-            $this->out->writeln("Unable to Fetch fresher-candidates, error: ".$e->getMessage());
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to fetch fresher-candidates! error: ".$e->getMessage());
             return response()->json(['success'=>false, "message"=>"Unable to Fetch fresher-candidates!", "error"=>$e->getMessage()], $this->failedStatus);
         }
     }
 
     public function cacheIn($round_id, $value){
         try{
-            $this->out->writeln("********* CAching round candidates!");
             Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Caching round: $round_id");
             $key = $this->cacheKey.$round_id;
             CacheCandidates::forget($key);
             CacheCandidates::put($key, $value);
             Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Caching successful.");
         }catch (\Exception $e){
-            $this->out->writeln("Unable to cache!error: ".$e->getMessage());
             Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to cached! error: ".$e->getMessage());
             return false;
         }
@@ -104,7 +106,6 @@ class RoundCandidatesController extends Controller
     }
 
     public function store(Request $request){
-        $this->out->writeln('Storing Candidates based on the round-id');
         try{
             Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Storing Candidates to round(id): ".$request['round_id']);
             request()->validate([
@@ -133,24 +134,26 @@ class RoundCandidatesController extends Controller
             if(!is_null($confirm_candidates)){
                 $round = Round::with('question_set')->where('id',$input['round_id'])->first();
                 $temp_string = substr($confirm_candidates,0,strlen($confirm_candidates)-1);
-                $candidates = $this->candidatesOnly($round->id);
-                $this->cacheIn($round->id, $candidates);
+//                $candidates = $this->candidatesOnly($round->id);
+//                $this->cacheIn($round->id, $candidates);
                 $this->roundConfirmFromServer($round, $confirm_candidates);
 //                if (CacheCandidates::has($this->cacheKey.$round->id)) {
 //                    return CacheCandidates::get($this->cacheKey.$round->id);
 //                }
+                Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Successfully stored Candidates to round(id): ".$request['round_id']);
                 return response()->json(['success'=>true, 'confirmed_candidates'=>$confirm_candidates, 'failed_candidates'=>$failed_candidates],$this->successStatus);
             }
-//            return $this->candidatesOnly();
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Failed to store Candidates to round(id): ".$request['round_id']);
             return response()->json(['success'=>false, 'confirmed_candidates'=>$confirm_candidates, 'failed_candidates'=>$failed_candidates], $this->failedStatus);
         }catch (\Exception $e){
-            $this->out->writeln("Unable to Enroll the Round-candidates, error");
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to enroll Round-candidates! error: ".$e->getMessage());
             return response()->json(['success'=>false, "message"=>"Unable to Enroll the Round-candidates", "error"=>$e->getMessage()], $this->failedStatus);
         }
     }
 
     public function roundConfirmMail($round, $users){
         try{
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Confirming round-candidates!");
             $round_name = $round->name;
             $title = $round_name;
             if(!is_null($round->question_set)){
@@ -173,12 +176,13 @@ class RoundCandidatesController extends Controller
             }
         }catch (\Exception $e){
             $this->out->writeln("Unable to Confirm student through email, error: ".$e->getMessage());
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."#Unable to confirm round-candidates!");
         }
     }
 
     public function roundConfirmFromServer($round, $candidates){
         try{
-            $this->out->writeln("Requesting email server for Bulk-email..");
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Sending information to email-server.");
             $delay = env("EMAIL_SERVER_JOB_DELAY");
             $url = env("EMAIL_SERVER_URL").'assessment-confirmation';
             $home_url = env('FRONT_END_HOME');
@@ -204,44 +208,58 @@ class RoundCandidatesController extends Controller
             $this->out->writeln("Url: $url");
             if($response->getStatusCode()!=200)
                 throw new \Exception("Mail server is not responding, its may be down or something else!");
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Successfully sent confirmation info to email-server.");
             return true;
         }catch(\Exception $e){
-            $this->out->writeln("Unable to Confirm Student trough email, error: ".$e->getMessage());
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to sent round-confirm info to the candidates! error: ".$e->getMessage());
             return false;
         }
     }
 
     public function update(Request $request, $id){
-        $this->out->writeln('Updating Round Candidates...');
-        request()->validate([
-            'round_id'=>'required',
-            'student_id'=>'required',
-        ]);
-        $input = $request->all();
-        $data=[
-            'round_id'=>$input['round_id'],
-            'student_id'=>$input['student_id']
-        ];
-        if(RoundCandidates::where('round_id',$input['round_id'])->where('student_id','=',$input['student_id'])->where('id','!=',$id)->exists()){
-            return response()->json(['success'=>false, 'message'=>'Student is already valid candidate for this round!'],$this->invalidStatus);
-        }
-        $round_candidate =RoundCandidates::find($id);
-        if(!$round_candidate){
-            return response()->json(['success'=>false, 'message'=>'Round & candidate combination are not found with this id: '.$id],$this->invalidStatus);
-        }
-        $round_candidate->update($data);
-        if($round_candidate->save()){
+        try{
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Updating Round-candidate");
+            request()->validate([
+                'round_id'=>'required',
+                'student_id'=>'required',
+            ]);
+            $input = $request->all();
+            $data=[
+                'round_id'=>$input['round_id'],
+                'student_id'=>$input['student_id']
+            ];
+            if(RoundCandidates::where('round_id',$input['round_id'])->where('student_id','=',$input['student_id'])->where('id','!=',$id)->exists()){
+                Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Student is already valid candidate for this round!");
+                return response()->json(['success'=>false, 'message'=>'Student is already valid candidate for this round!'],$this->invalidStatus);
+            }
+            $round_candidate =RoundCandidates::find($id);
+            if(!$round_candidate){
+                Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Round & Candidate combination is not found!");
+                return response()->json(['success'=>false, 'message'=>'Round & candidate combination are not found with this id: '.$id],$this->invalidStatus);
+            }
+//            $round_candidate->update($data);
+            if(!$round_candidate->update($data))
+                throw new \Exception("Unable to update round-candidates!");
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Successfully upgraded round-candidates.");
             return response()->json(['success'=>true, 'round_candidate'=>$round_candidate],$this->successStatus);
+        }catch(\Exception $e){
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to upgrade round-candidates! error: ".$e->getMessage());
+            return response()->json(["success"=>false, "message"=>"Failed to upgrade round-candidates!", "error"=>$e->getMessage()], $this->failedStatus);
         }
-        return response()->json(['success'=>false, 'message'=>'unable to update round candidates, id: '.$input['id']], $this->failedStatus);
     }
     public function show($id){
-        $this->out->writeln('Fetching round with candidates, id: '.$id);
-        $round_candidate = RoundCandidates::find($id);
-        if($round_candidate){
-            return response()->json(['success'=>true, 'round_candidate'=>$round_candidate],$this->successStatus);
+        Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# ");
+        try{
+            $round_candidate = RoundCandidates::find($id);
+            if($round_candidate){
+                Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetched round-with candidate successfully!");
+                return response()->json(['success'=>true, 'round_candidate'=>$round_candidate],$this->successStatus);
+            }
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Round with candidate not found!");
+            return response()->json(['success'=>false, 'message'=>'Round with candidates are not found!, id: '.$id], $this->invalidStatus);
+        }catch (\Exception $e){
+            return response()->json(['success'=>false, "message"=>"Fetching round-candidate is unsuccessful!", "error"=>$e->getMessage()], $this->failedStatus);
         }
-        return response()->json(['success'=>false, 'message'=>'Round with candidates are not found!, id: '.$id], $this->invalidStatus);
     }
 
     public function destroy($id){
@@ -258,16 +276,15 @@ class RoundCandidatesController extends Controller
 
     public function eachRoundCandidates($round_id){
         try{
-            $this->out->writeln('Fetching candidates based on the round-id: '.$round_id);
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Fetching Each round-candidates with round-id: ".$round_id);
             $round_candidates = RoundCandidates::with('user_profile','academic_info')->where('round_id',$round_id)->get();
             if(!$round_candidates)
-                return response()->json(['success'=>false, "message"=>"Round Candidates with round-id not found!"], $this->invalidStatus);
+                throw new \Exception("Not found candidates");
+            Log::channel("ac_info")->info(__CLASS__."@".__FUNCTION__."# Successfully Fetched round-candidates.");
             return response()->json(['success'=>true, 'round_candidates'=>$round_candidates],$this->successStatus);
         }catch (\Exception $e){
-            $this->out->writeln("Unable to find candidates with this round-id: ".$round_id.", error".$e->getMessage());
+            Log::channel("ac_error")->info(__CLASS__."@".__FUNCTION__."# Unable to fetch round-candidates! error: ".$e->getMessage());
             return response()->json(['success'=>false, 'message'=>'Unable to find candidates with this round-id: '.$round_id, "error"=>$e->getMessage()],$this->failedStatus);
         }
-
     }
-
 }
