0e68dd721195a86b6fc138c6dd6c1dc4b5658ddd
Mohammad Hemayet Ullah <40654463+hemayet-nirjhoy@users.noreply.github.com>
2022-07-27 15:30:16 +0600

    Question creation is reformed for tag

--- app/Http/Controllers/API/Question/QuestionController.php
+++ app/Http/Controllers/API/Question/QuestionController.php
@@ -77,6 +77,136 @@ class QuestionController extends Controller
         //        return response()->json(['success' => true, 'questions' => $questions], $this-> successStatus);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     * Developed specially for vue
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+
+    public function store(Request $request)
+    {
+
+        Log::info(explode(',',  $request['tags']));
+
+        // exit();
+
+
+
+        request()->validate([
+            'question_text' => 'required',
+        ]);
+        $input = $request->all();
+
+        // $this->out->writeln('data: ' . $input);
+
+
+        $contributor = Contributor::where('profile_id', $input['profile_id'])->first();
+
+        // Check contributor
+        if (!$contributor)
+            return response()->json(['success' => false, 'message' => 'Contributor not found'], $this->invalidStatus);
+
+        // Check question category
+        // $questionCategory = QuestionCategory::find($input['category_id']);
+        // if (!$questionCategory)
+        //     return response()->json(['success' => false, 'message' => 'Question Category not found'], $this->invalidStatus);
+
+        // Check answer
+        if (!$input['answer'])
+            return response()->json(['success' => false, 'message' => 'No answer selected'], $this->invalidStatus);
+
+        $user = Auth::user();
+        $userProfile = UserProfile::where('user_id', $user->id)->first();
+
+        $institute_id = NULL;
+        if ($userProfile->institute_id) {
+            $institute_id = $userProfile->institute_id;
+        }
+
+        // Add question
+        $questionData = [
+            'institute_id' => $institute_id,
+            //'profile_id' => $input['profile_id'],
+            'profile_id' => $userProfile->id,
+            'category_id' => $input['category_id'],
+            'privacy' => (!empty($_POST["privacy"])) ? $input['privacy'] : 0,
+            'publish_status' => $input['publish_status'],
+            'question_type' => $input['question_type'],
+            'question_text' => $input['question_text'],
+            'description' => $input['description'],
+            'option_type' => $input['option_type'],
+            'no_of_option' => $input['no_of_option'],
+            'no_of_answer' => $input['no_of_answer'],
+            'no_of_used' => $input['no_of_used'],
+            'no_of_comments' => $input['no_of_comments'],
+            'average_rating' => $input['average_rating'],
+            'img' => $input['img'],
+            'active' => $input['active'],
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+        ];
+
+        // $this->out->writeln($questionData);
+        // dd($questionData);
+        $question = Question::create($questionData);
+
+        // Add question options
+        $questionOptionData = [];
+        $serial_no_data = explode('|', $input['serial_no']);
+        // $option_data = explode('|', $input['option']);
+        $option_data = $input['option'];
+        // $description_data = explode('|', $input['description']);
+        $description_data = $input['description'];
+
+        $image_data = explode('|', $input['img']);
+        for ($i = 0; $i < $input['no_of_option']; $i++) {
+            $questionOptionData = [
+                'question_id' => $question->id,
+                'serial_no' => $serial_no_data[$i],
+                'option' => $option_data[$i],
+                //'description' => $description_data[$i],
+                //'img' => $image_data[$i],
+            ];
+            QuestionDetail::create($questionOptionData);
+        }
+
+        // Add question answer
+        $no_of_ans = $request['no_of_answer'];
+        // for ($i = 0; $i < $no_of_ans; $i++) {
+        $questionAnswerData = [
+            'question_id' => $question->id,
+            'answer' => $input['answer'],
+            'reference' => $input['reference'],
+        ];
+        // $this->writeln(questionAnswerData);
+        QuestionAnswer::create($questionAnswerData);
+        // }
+
+
+        // Add question tags
+        $tag_data = explode(',',  $input['tags']);
+        if ($tag_data) {
+            for ($i = 0; $i < count($tag_data); $i++) {
+                $questionTagData = [
+                    'question_id' => $question->id,
+                    'category_id' => $tag_data[$i],
+                ];
+                QuestionCategoryTag::create($questionTagData);
+            }
+
+            Log::info($questionTagData);
+        }
+
+        // Increment contributor total no of question
+        Contributor::find($contributor->id)->increment('total_question');
+
+        if ($question)
+            return response()->json(['success' => true, 'question' => $question], $this->successStatus);
+        else
+            return response()->json(['success' => false, 'message' => 'Question added fail'], $this->failedStatus);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -84,7 +214,7 @@ class QuestionController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store_before_vue_integrations(Request $request)
     {
 
         Log::info($request['description']);
