4c522871a578cc073ff3a5a769f850a957ba9d4f
saifmahmud <saif.mahmud@neural-semiconductor.com>
2023-01-02 12:18:11 +0600

    question category seeder

---
+++ database/seeds/QuestionCategorySeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+use App\QuestionCategory;
+use Illuminate\Database\Seeder;
+
+class QuestionCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        QuestionCategory::create([
+            [
+                'name' => 'Advance',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'C++',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'PHP',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'Python',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'JAVA',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'JavaScript',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'Analog',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'DFT',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+            [
+                'name' => 'PNR',
+                'parents_id' => 0,
+                'layer' => 0,
+                'description' => NULL,
+            ],
+        ]);
+    }
+}
